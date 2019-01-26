1abdd402a82929452ba6ef1410e7b7838ce6a065
Draško Kokić <drasko.kokic@gmail.com>
2019-01-26 15:26:53 +0100

    [client] Add routing for dashboard, setting and hello-world

---
+++ client/src/router.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Dashboard from './components/Dashboard'
+import HelloWorld from './components/HelloWorld'
+import Settings from './components/Settings'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/dashboard', component: Dashboard },
+  { path: '/settings', component: Settings },
+  { path: '/hello-world', component: HelloWorld },
+  { path: '*', redirect: '/hello-world' }
+]
+
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
+
+export default router
