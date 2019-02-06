1be32e29898311ebad32a0d9980d62fcfc4f09d4
Michael Reith <mreith@Michaels-iMac.local>
2019-02-06 15:50:44 +0100

    added Homes component

---
+++ client/src/components/Homes.vue
@@ -0,0 +1,40 @@
+
+<template>
+  <div>
+    <h4 v-if="loading">Loading...</h4>
+    <div>Homes</div>
+    <div v-for="home in allHomes" :key="home.id">{{home.description}}</div>
+  </div>
+</template>
+
+<script>
+import { ALL_HOMES_QUERY } from "../graphql";
+export default {
+  name: "Homes",
+  props: {
+    title: String
+  },
+  data() {
+    return {
+      // 3
+      allHomes: [],
+      loading: 0
+    };
+  },
+  apollo: {
+    homes: {
+      query: ALL_HOMES_QUERY,
+      result({ data, loading }) {
+        if (!loading) {
+          this.allHomes = data.homes;
+        }
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
+
+
