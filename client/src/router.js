623242884f0a28188ec26073401af2d3009b48b6
Michael Reith <mreith@Michaels-iMac.local>
2019-02-06 15:51:13 +0100

    added homes route

--- client/src/router.js
+++ client/src/router.js
@@ -1,22 +1,24 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Cats from './components/Cats'
-import Dashboard from './components/Dashboard'
-import HelloWorld from './components/HelloWorld'
-import Settings from './components/Settings'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Cats from "./components/Cats";
+import Homes from "./components/Homes";
+import Dashboard from "./components/Dashboard";
+import HelloWorld from "./components/HelloWorld";
+import Settings from "./components/Settings";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-  { path: '/cats', component: Cats },
-  { path: '/dashboard', component: Dashboard },
-  { path: '/settings', component: Settings },
-  { path: '/hello-world', component: HelloWorld },
-  { path: '*', redirect: '/hello-world' }
-]
+  { path: "/homes", component: Homes },
+  { path: "/cats", component: Cats },
+  { path: "/dashboard", component: Dashboard },
+  { path: "/settings", component: Settings },
+  { path: "/hello-world", component: HelloWorld },
+  { path: "*", redirect: "/hello-world" }
+];
 
 const router = new VueRouter({
   routes // short for `routes: routes`
-})
+});
 
-export default router
+export default router;
