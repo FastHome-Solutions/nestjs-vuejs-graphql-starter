9aa2cca38e5c4ed5934344c5f12f0c2a4ae24a9d
Michael Reith <mreith@Michaels-iMac.local>
2019-02-07 10:34:23 +0100

    added getHomes Query

--- client/src/graphql.js
+++ client/src/graphql.js
@@ -3,3 +3,6 @@ import gql from 'graphql-tag'
 export const ALL_CATS_QUERY = gql`
   query {getCats {id name age}}
 `
+export const ALL_HOMES_QUERY = gql`
+  query {homes {id headlineExpose description}}
+`
--- client/src/main.js
+++ client/src/main.js
@@ -1,38 +1,50 @@
-import { ApolloClient } from 'apollo-client'
-import { HttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import VueApollo from 'vue-apollo'
-import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { setContext } from "apollo-link-context";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import VueApollo from "vue-apollo";
+import Vue from "vue";
+import "./plugins/vuetify";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 const httpLink = new HttpLink({
   // You should use an absolute URL here
-  uri: 'http://localhost:9080/graphql'
-})
+  uri: "https://fasthome-prisma-543c4c0660.herokuapp.com/fasthome-prisma/dev"
+});
+
+const authLink = setContext((_, { headers }) => {
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization:
+        "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NDk1NTA4MDgsIm5iZiI6MTU0OTQ2NDQwOH0.OUUh58B85CQ9UFBptCok-hp_hK4-KSXAkQof-uaiUBk"
+    }
+  };
+});
 
 const apolloClient = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
   connectToDevTools: true
-})
+});
 
-Vue.use(VueApollo)
+Vue.use(VueApollo);
 
 const apolloProvider = new VueApollo({
   defaultClient: apolloClient,
   defaultOptions: {
-    $loadingKey: 'loading'
+    $loadingKey: "loading"
   }
-})
+});
 
 new Vue({
   provide: apolloProvider.provide(),
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
